e58c0c0b20a54773fc88f08ad427ea77e1b8b134
aahilmouzoune-web <aahilmouzoune@gmail.com>
2026-06-14 20:24:06 +0100

    Add files via upload

---
+++ Class 3.py
@@ -0,0 +1,13 @@
+import pygame
+import random
+
+pygame.init()
+
+SPRITE_COLOR_CHANGE_EVENT=pygame.USEREVEVNT+1
+BACKROUND_COLOR_CHANGE_EVENT==pygame.USERVENT+2
+BLUE =pygame.Color('blue')
+LIGHTBLUE=pygame.Color('lightblue')
+
+YELLOW=pygame.color('yellow')
+MAGENTA=pygame.color('magenta')
+ORANGE=pygame.color('orange')
